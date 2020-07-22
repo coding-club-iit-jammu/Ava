35171605770d66d043c9b1d5888d42c50b036723
Abhishek <abhishekchaudhary0220@gmail.com>
2020-07-22 21:29:33 +0530

    Azure Pipelines

--- codingclubvm.sh
+++ codingclubvm.sh
@@ -1,5 +1,6 @@
 cp -r /home/azureuser/azagent/_work/r1/a/_abhishek0220_Ava/* /home/azureuser/Ava/
 cd /home/azureuser/Ava/
-source virtualenv_name/bin/activate
-pip install -r reqirements.txt
-python ava.py
+sudo pm2 delete ava
+sudo pm2 -f start ava.py --no-autorestart
+sudo pm2 startup systemd
+sudo pm2 save
