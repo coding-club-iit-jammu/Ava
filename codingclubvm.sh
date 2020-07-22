1bd9dc9d64566c8d02e351455e18aab433baaf08
Abhishek <abhishekchaudhary0220@gmail.com>
2020-07-22 22:11:09 +0530

    Azure Pipelines3

--- codingclubvm.sh
+++ codingclubvm.sh
@@ -2,6 +2,6 @@ sudo pm2 delete ava
 cp -r /home/azureuser/azagent/_work/r1/a/_abhishek0220_Ava/* /home/azureuser/Ava/
 cd /home/azureuser/Ava/
 pip3 install -r reqirements.txt
-sudo pm2 -f start ava.py --no-autorestart
+sudo pm2 -f start ava.py --no-autorestart --interpreter python3
 sudo pm2 startup systemd
 sudo pm2 save
