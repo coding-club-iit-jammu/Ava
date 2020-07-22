b2ed2022d624bcfc6c6a47402bb95185ed3c3c50
Abhishek <abhishekchaudhary0220@gmail.com>
2020-07-22 10:38:21 +0530

    pipeline

--- codingclubvm.sh
+++ codingclubvm.sh
@@ -1,1 +1,5 @@
+cp -r /home/azureuser/azagent/_work/r1/a/_abhishek0220_Ava/* /home/azureuser/Ava/
+cd /home/azureuser/Ava/
+source virtualenv_name/bin/activate
+pip install -r reqirements.txt
 python ava.py
