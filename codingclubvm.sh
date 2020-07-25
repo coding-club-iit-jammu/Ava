a1dbbd7e4f0a5c77eb4c9a56ba9fc7822f2dac25
Abhishek <abhishekchaudhary0220@gmail.com>
2020-07-25 23:02:46 +0530

    info mobile bug resolved

--- codingclubvm.sh
+++ codingclubvm.sh
@@ -1,6 +1,4 @@
 sudo pm2 delete ava
-sudo pm2 startup systemd
-sudo pm2 save
 cp -r /home/azureuser/azagent/_work/r1/a/_abhishek0220_Ava/* /home/azureuser/Ava/
 cd /home/azureuser/Ava/
 pip3 install -r reqirements.txt
--- script/info.py
+++ script/info.py
@@ -37,12 +37,17 @@ class Info(commands.Cog):
 
     @commands.command()
     @commands.cooldown(1, 15, commands.BucketType.channel)
-    async def info(self, ctx,member : str, ver_type=""):
+    async def info(self, ctx,member : str):
         if(len(member) < 3):
             return await ctx.send(f"{ctx.author.mention} Pls enter minimum of 3 charaters")
         con = False
         if(re.search("^<@!.*>$", member)):
             userid = member[3:-1]
+            con = True
+        elif(re.search("^<@.*>$", member)):
+            if(member[2] == '&'):
+                return await ctx.send(f"{ctx.author.mention} this command does not support roles as of now")
+            userid = member[2:-1]
             con = True
         elif(member.isnumeric()):
             userid = member
@@ -53,7 +58,7 @@ class Info(commands.Cog):
             out = ""
             for user in users:
                 users_len += 1
-                out = out + f"Name : {user['name']}\nEntry Number : {user['entry']}\nDiscord Id : {user['discordid']}\n\n"
+                out = out + f"Name : {user['name']}\nEntry Number : {user['entry']}\nDiscord Id : {user['discordid']}\nUserName : {user['username']}\n\n"
             if(out == ""):
                 out = "\tSorry no Member found"
             out = "```" + out + "```"
@@ -61,38 +66,21 @@ class Info(commands.Cog):
             if(users_len > 1):
                 dev_role = discord.utils.get(guild.roles, name="Core Team") 
                 await logs.print(f"{dev_role.mention} Error occured in searching userid {userid}. If you seeing this plz report this to ADMIN ASAP.")  
-        elif(ver_type.lower() == "name"):
-            all_users = db.member.find()
-            users = []
-            for user in all_users:
-                if(match(member, user['name'])):
-                    users.append(user)
-            out = ""
-            for user in users:
-                out = out + f"Name : {user['name']}\nEntry Number : {user['entry']}\nDiscord Id : {user['discordid']}\n\n"
-            if(len(users) == 0):
-                out = "\tSorry no Member found"
-            else:
-                out = f"Found total {len(users)}\n\n" + out
-            out = "```" + out + "```"
-            await ctx.send(out) 
-        elif(ver_type.lower() == "user"):
-            all_users = db.member.find()
-            users = []
-            for user in all_users:
-                if(match(member, user['username'])):
-                    users.append(user)
-            out = ""
-            for user in users:
-                out = out + f"Name : {user['name']}\nEntry Number : {user['entry']}\nDiscord Id : {user['discordid']}\n\n"
-            if(len(users) == 0):
-                out = "\tSorry no Member found"
-            else:
-                out = f"Found total {len(users)}\n\n" + out
-            out = "```" + out + "```"
-            await ctx.send(out) 
         else:
-            await ctx.send(f"{ctx.author.mention} Pls mention the user or his discord id as a parameter or correct ver_type, user for username and name for name")
+            all_users = db.member.find()
+            users = []
+            for user in all_users:
+                if(match(member, user['name']) or match(member, user['username'])):
+                    users.append(user)
+            out = ""
+            for user in users:
+                out = out + f"Name : {user['name']}\nEntry Number : {user['entry']}\nDiscord Id : {user['discordid']}\nUserName : {user['username']}\n\n"
+            if(len(users) == 0):
+                out = "\tSorry no Member found"
+            else:
+                out = f"Found total {len(users)}\n\n" + out
+            out = "```" + out + "```"
+            await ctx.send(out) 
 def setup(bot):
     print("info command added")   
     bot.add_cog(Info(bot))
