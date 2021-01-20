9ee61835a58e799e907d74774c2d374c4c64703c
Abhishek Chaudhary <abhishekchaudhary0220@gmail.com>
2021-01-20 22:16:39 +0530

    venv

--- codingclubvm.sh
+++ codingclubvm.sh
@@ -2,7 +2,9 @@ pwd
 sudo pm2 delete ava
 cp -r /home/azureuser/azagent/_work/r1/a/_abhishek0220_Ava/* /home/azureuser/Ava/
 cd /home/azureuser/Ava/
-pip3 install -r reqirements.txt
-sudo pm2 -f start ava.py --no-autorestart --interpreter python3
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+sudo pm2 -f start ava.py --no-autorestart --interpreter /home/azureuser/Ava/venv/bin/python
 sudo pm2 startup systemd
 sudo pm2 save
